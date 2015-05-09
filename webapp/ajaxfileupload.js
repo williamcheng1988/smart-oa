d71c4c7c8af449242af5fd0dfa1908aaa1026716
wesson <wesson@ae9294a5-661a-4322-a638-bfd76ec41a80>
2015-05-09 09:12:57 +0000

    git-svn-id: svn://112.74.73.69/smartoa_project@55 ae9294a5-661a-4322-a638-bfd76ec41a80

--- webapp/ajaxfileupload.js
+++ webapp/ajaxfileupload.js
@@ -1,6 +1,14 @@
 
 jQuery.extend({
 	
+	handleError: function( s, xhr, status, e ) {
+        // If a local callback was specified, fire it
+        if ( s.error )
+            s.error( xhr, status, e );
+        // If we have some XML response text (e.g. from an AJAX call) then log it in the console
+        else if(xhr.responseText)
+            console.log(xhr.responseText);
+    },
 
     createUploadIframe: function(id, uri)
 	{
@@ -191,10 +199,11 @@ jQuery.extend({
         // Get the JavaScript object, if JSON is used.
         if ( type == "json" )
             eval( "data = " + data );
+        	//eval("data = \" "+data+" \" ");
         // evaluate scripts within html
         if ( type == "html" )
             jQuery("<div>").html(data).evalScripts();
-
+        
         return data;
     }
 })
