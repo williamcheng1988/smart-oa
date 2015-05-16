f74eb490c2ac03eb2a2e11d4beb07ab82f944af5
william <william@ae9294a5-661a-4322-a638-bfd76ec41a80>
2015-05-16 10:28:49 +0000

    算法优化
    git-svn-id: svn://112.74.73.69/smartoa_project@109 ae9294a5-661a-4322-a638-bfd76ec41a80

--- webapp/widgets/calculator/calculator.js
+++ webapp/widgets/calculator/calculator.js
@@ -84,7 +84,8 @@ function replacePattern(target,resources,keyAndvalue){
 	}
 	var mode;
 	if(target.hasClass('easyui-numberbox')){
-		mode = eval(resources);
+		//mode = eval(resources);
+		mode = calcArith(resources);
 	}else{
 		mode = resources.split('+').join('');
 		//mode = resources;
@@ -120,4 +121,110 @@ function upDigit(n){
 		s = p.replace(/(零.)*零$/, '').replace(/^$/, '零')  + unit[0][i] + s; 
 	} 
 	return head + s.replace(/(零.)*零元/, '元').replace(/(零.)+/g, '零').replace(/^整$/, '零元整'); 
+}
+//计算值
+function calcArith(strV) {
+	//if (("+-*/").indexOf(strV.substring(0, 1)) >= 0) {strV = strV.substring(1, strV.length);}
+	//计算加法
+	return exA(strV);
+}
+function exA(strV) {
+	var dJe = 0.0;
+	if (strV == "") {return dJe;}
+	if (strV.indexOf("+") > 0) {
+		var arryTemp = strV.split("+");
+		dJe = exP(arryTemp[0]);
+		for (var i = 1; i < arryTemp.length; i++) {
+			//dJe += exP(arryTemp[i]);
+			dJe=accAdd(dJe,exP(arryTemp[i]));
+		}
+	} else {
+		dJe = exP(strV);
+	}
+	return dJe;
+}
+//计算减法
+function exP(strV) {
+	var dJe = 0.0;
+	if (strV == "") {
+		return dJe;
+	}
+	if (strV.indexOf("-") > 0) {
+		var arryTemp = strV.split("-");
+		dJe = exX(arryTemp[0]);
+		for (var i = 1; i < arryTemp.length; i++) {
+			//dJe -= exX(arryTemp[i]);
+			dJe=accSub(dJe,exX(arryTemp[i]));
+		}
+	} else {
+		dJe = exX(strV);
+	}
+	return dJe;
+}
+//计算乘法
+function exX(strV) {
+	var dJe = 0.0;
+	if (strV == "") {
+		return dJe;
+	}
+	if (strV.indexOf("*") > 0) {
+		var arryTemp = strV.split("*");
+		dJe = exX(arryTemp[0]);
+		for (var i = 1; i < arryTemp.length; i++) {
+			//dJe *= exC(arryTemp[i]);
+			dJe=accMul(dJe,exC(arryTemp[i]));
+		}
+	} else {
+		dJe = exC(strV);
+	}
+	return dJe;
+}
+//计算除法
+function exC(strV) {
+	var dJe = 1.0;
+	if (strV == "") {
+		return dJe;
+	}
+	if (strV.indexOf("/") > 0) {
+		var arryTemp = strV.split("/");
+		dJe = exX(arryTemp[0]);
+		for (var i = 1; i < arryTemp.length; i++) {
+			//dJe /= parseFloat(arryTemp[i]);
+			dJe=accDiv(dJe,parseFloat(arryTemp[i]));
+		}
+	} else {
+		dJe = parseFloat(strV);
+	}
+	return dJe;
+}
+
+function accAdd(arg1,arg2){
+	var r1,r2,m;
+	try{r1=arg1.toString().split(".")[1].length;}catch(e){r1=0;}
+	try{r2=arg2.toString().split(".")[1].length;}catch(e){r2=0;}
+	m=Math.pow(10,Math.max(r1,r2));
+	return (parseInt(arg1*m)+parseInt(arg2*m))/m;
+}
+
+function accSub(arg1,arg2){
+	return accAdd(arg1,-arg2);
+}
+
+function accMul(arg1,arg2)
+{
+	var m=0,s1=arg1.toString(),s2=arg2.toString();
+	try{m+=s1.split(".")[1].length;}catch(e){}
+	try{m+=s2.split(".")[1].length;}catch(e){}
+	return Number(s1.replace(".",""))*Number(s2.replace(".",""))/Math.pow(10,m);
+}
+
+function accDiv(arg1,arg2){
+	var t1=0,t2=0,r1,r2;
+	try{t1=arg1.toString().split(".")[1].length;}catch(e){}
+	try{t2=arg2.toString().split(".")[1].length;}catch(e){}
+	with(Math){
+		r1=Number(arg1.toString().replace(".",""));
+		r2=Number(arg2.toString().replace(".",""));
+		return (r1/r2)*pow(10,t2-t1);
+	}
 }
