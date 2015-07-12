ea029a250ea57f52a8185a5edcdbdd2ad8437b15
wesson <wesson@ae9294a5-661a-4322-a638-bfd76ec41a80>
2015-07-12 06:09:51 +0000

    返回页面Bug修改
    git-svn-id: svn://112.74.73.69/smartoa_project@315 ae9294a5-661a-4322-a638-bfd76ec41a80

--- webapp/themes/themes.js
+++ webapp/themes/themes.js
@@ -105,3 +105,11 @@ function switchTopic(action){
 	}
 }
 
+function GetQueryParam(name){
+	var reg = new RegExp("(^|&)"+name+"=([^&]*)(&|$)");
+	var r = window.location.search.substr(1).match(reg);
+	if(r != null){
+	  return unescape(r[2]);
+	}
+	return null;
+}
